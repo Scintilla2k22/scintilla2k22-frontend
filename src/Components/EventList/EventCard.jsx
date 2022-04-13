ad9a456c9ebc6c91f21ab9f9a2d737c0c9cd3726
MohdAnas07 <anas98144@gmail.com>
2022-04-13 22:37:23 +0530

    event section

--- src/Components/EventList/EventCard.jsx
+++ src/Components/EventList/EventCard.jsx
@@ -5,12 +5,12 @@ const loc = process.env.PUBLIC_URL
 function EventCard() {
     return (
         <div>
-            <div className="card" style={{ width: "18rem" }}>
-                <img src={`${loc}/image/knowbuddy.png`} className="card-img-top" alt="..." />
+            <div className="card">
+                <img src={`${loc}/image/knowbuddy.png`} className="card-img-top" alt="event-card" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-success">Go somewhere</a>
+                    <p className="card-text">Some quick example</p>
+                    <a href='https://www.facebook.com' className="btn btn-success">Check Out</a>
                 </div>
             </div>
         </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,6 +5,7 @@ import Ipad from '../Arts/ipad'
 function Header() {
     return (
         <div className='header'>
+            <h1 style={{ textAlign: 'center' }} className='heading1'>Scintilla 2K22</h1>
             <Ipad />
         </div>
     )
