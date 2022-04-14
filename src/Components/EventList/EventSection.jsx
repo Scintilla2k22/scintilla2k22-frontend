06ac0e15b60bc7f4937cfc238674840957f2a3ce
MohdAnas07 <anas98144@gmail.com>
2022-04-14 17:21:03 +0530

    new commit

--- src/Components/EventList/EventSection.jsx
+++ src/Components/EventList/EventSection.jsx
@@ -2,21 +2,28 @@ import React from 'react'
 import './event.css'
 import EventCard from './EventCard'
 
+const color = {
+    upcoming: 'red',
+    live: 'green',
+    ended: 'gray'
+}
+
 function EventSection(props) {
 
-   const {
-       data = null,
-   } = props;
+    const {
+        data = null,
+        title
+    } = props;
 
-   console.log("data - ", data)
+    console.log("data - ", data)
     return (
         <div className='event-list-container'>
-            <h2 style={{ color: 'red' }} className='heading2' >Upcoming Events</h2>
+            <h2 style={{ color: color[title] }} className='heading2' >{title} Events</h2>
             <div className='event-list'>
-                
-                { data && data.map((info, ind) =>  <EventCard data = {info} />
-                ) }
-                
+
+                {data && data.map((info, ind) => <EventCard data={info} />
+                )}
+
             </div>
         </div>
     )
