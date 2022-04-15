e50ebc243bfaac957673391b5f410840e19ed9f3
MohdAnas07 <anas98144@gmail.com>
2022-04-15 16:41:44 +0530

    event card

--- src/Components/EventList/EventCardNew.jsx
+++ src/Components/EventList/EventCardNew.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './eventNew.scss'
+import { Link } from 'react-router-dom';
+
+const loc = process.env.PUBLIC_URL
 
 const EventCardNew = () => {
     return (
         <div className='event-card-new-container' >
-            
+            <div className="event-card-left">
+                <img src={`${loc}/image/cardimg.jpg`} alt="imaheeeeee" />
+            </div>
+            <div className="event-card-right">
+                <div className='card-right-info'>
+                    <h3 className='heading2'>Dancing</h3>
+                    <p>15-04-22</p>
+                </div>
+                <Link to='events/eventpage'>
+                    <button>Event Info</button>
+                </Link>
+            </div>
         </div>
     );
 }
---
+++ src/Components/events/LiveEvent/EventCarousel.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import EventCardNew from '../../EventList/EventCardNew'
+import EventsRow from '../../EventList/EventsRow'
+import './eventcarousel.css'
+
+function EventCarousel() {
+    return (
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <EventsRow />
+                </div>
+                <div className="carousel-item">
+                    <EventsRow />
+                </div>
+                <div className="carousel-item">
+                    <EventsRow />
+                </div>
+                <div className="carousel-item">
+                    <EventsRow />
+                </div>
+                <div className="carousel-item">
+                    <EventsRow />
+                </div>
+                <div className="carousel-item">
+                    <EventsRow />
+                </div>
+            </div>
+
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
+
+export default EventCarousel
+
+
+
+{/* <div className="container">
+            <div className="carousel">
+                <div className="carousel__face"><span>This is something</span></div>
+                <div className="carousel__face"><span>Very special</span></div>
+                <div className="carousel__face"><span>Special is the key</span></div>
+                <div className="carousel__face"><span>For you</span></div>
+                <div className="carousel__face"><span>Just give it</span></div>
+                <div className="carousel__face"><span>A try</span></div>
+                <div className="carousel__face"><span>And see</span></div>
+                <div className="carousel__face"><span>How IT Works</span></div>
+                <div className="carousel__face"><span>Woow</span></div>
+            </div>
+        </div> */}
