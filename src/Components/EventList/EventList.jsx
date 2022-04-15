e155d9e277283e20026d406fd7ecb6996d9400cb
MohdAnas07 <anas98144@gmail.com>
2022-04-15 14:58:17 +0530

    resolve conflict

--- src/Components/EventList/EventList.jsx
+++ src/Components/EventList/EventList.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { EventContext } from '../../context'
-import './event.css'
-import EventCard from './EventCard'
+import '../Home/HeadSection/headSection.scss'
 import EventSection from './EventSection'
+import './event.css'
+
 
 function EventList() {
 
     const [up_event, setUpEvent] = useState([])
     const [live_event, setLiveEvent] = useState([])
     const [ended_event, setEndedEvent] = useState([])
+
     const { state: {
         upcomming_events,
         live_events,
@@ -28,9 +30,9 @@ function EventList() {
         <div className='event'>
             <h1>Events</h1>
             <div className='event-type  m-container'>
-                <EventSection data={live_event} title='live' />
-                <EventSection data={up_event} title='upcoming' />
-                <EventSection data={ended_event} title='ended' />
+                {live_event.length > 0 && <EventSection data={live_event} title='live' />}
+                {up_event.length > 0 && <EventSection data={up_event} title='upcoming' />}
+                {ended_event.length > 0 && <EventSection data={ended_event} title='ended' />}
             </div>
         </div >
     )
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeadSection from './HeadSection/headSection'
+
+function Home() {
+    return (
+        <div >
+            <HeadSection />
+        </div>
+    )
+}
+
+export default Home
