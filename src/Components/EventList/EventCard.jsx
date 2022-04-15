d9d4068e54860bed7e0fbe58b1a7e70a6a0eceb1
Ayush Bisht <bisht.ayush2001@gmail.com>
2022-04-15 23:01:27 +0000

    event page listed , home pages styled , date added and checkout button also

--- src/Components/EventList/EventCard.jsx
+++ src/Components/EventList/EventCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { server } from '../../context/server';
 // import './event.scss'
 import './card.scss'
-
+import {CgMoreO} from 'react-icons/cg'
 // const loc = process.env.PUBLIC_URL
 
 function EventCard(props) {
@@ -15,15 +15,15 @@ function EventCard(props) {
     return (
 
         <div className="card-block">
-            <h1>{data.e_name}</h1>
+            <h1  >{data.e_name}</h1>
 
             <img src="https://images.pexels.com/photos/1680140/pexels-photo-1680140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
             <div className='card-info'>
-                <h4>
+                {/* <h4>
                     More Info
                 </h4>
-                <p>{data.e_desc}</p>
-
+                <p>{data.e_desc}</p> */}
+                <CgMoreO />
                 <Link to='/events/eventpage'>
                     <button>
                         Event Info
