b1841da9e2b5e074820dec04ca293d077cf26c72
Ayush Bisht <bisht.ayush2001@gmail.com>
2022-04-16 23:22:00 +0530

    route fixed checkpoint

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { EventProvider } from "./context";
+import { createBrowserHistory } from 'history';  
+
+const history = createBrowserHistory()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename= {process.env.PUBLIC_URL} history = {history} >
       <EventProvider>
         <App />
       </EventProvider>
