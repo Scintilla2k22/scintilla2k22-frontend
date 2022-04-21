a654621ce1c3a1eb1739cd0190df883a0271711e
MohdAnas07 <anas98144@gmail.com>
2022-04-21 14:14:31 +0530

    last updates

--- src/utils/eventImg.js
+++ src/utils/eventImg.js
@@ -1,4 +1,4 @@
-import { asset } from "."; 
+import { asset } from ".";
 
 export const getEventSvg = (code) => {
 
@@ -9,37 +9,36 @@ export const getEventSvg = (code) => {
 
     case "SING":
       return `${asset}/image/sci-assets/singing.svg`;
-      
 
     case "SKIT":
       return `${asset}/image/sci-assets/singing.svg`;
 
     case "POET":
-      return         `${asset}/image/sci-assets/poetry.svg`;
-      
+      return `${asset}/image/sci-assets/poetry.svg`;
 
     case "TALE":
-      return         `${asset}/image/sci-assets/talenthunt.svg`;
-      
+      return `${asset}/image/sci-assets/talenthunt.svg`;
 
     case "CAMP":
-      return   `${asset}/image/sci-assets/poetry.svg`;
-      
+      return `${asset}/image/sci-assets/poetry.svg`;
 
     case "FACE":
-      return         `${asset}/image/sci-assets/painting.svg`;
-      
+      return `${asset}/image/sci-assets/painting.svg`;
 
     case "FRAM":
       return `${asset}/image/sci-assets/framedesining.svg`;
-      
 
     case "TREA":
       return `${asset}/image/sci-assets/treasurehunt.svg`;
-      
+
+    case "DRAM":
+      return `${asset}/image/sci-assets/treasurehunt.svg`;
+
+    case "MAND":
+      return `${asset}/image/sci-assets/treasurehunt.svg`;
 
     default:
       return `${asset}/image/sci-assets/talenthunt.svg`;
-      
+
   }
 };
